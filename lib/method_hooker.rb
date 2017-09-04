ec75a0eee9e6928f85625e983533f441c7fc3947
nyaahara <d.niihara@gmail.com>
2017-09-04 23:46:28 +0800

    typo

--- lib/method_hooker.rb
+++ lib/method_hooker.rb
@@ -5,7 +5,7 @@ class Class
   @@hooked = []
   def pre_execute_method(execute_method_name, hook_method_name)
     return if @@hooked.any?{|hook| hook.strategy == :pre && hook.execute_method_name == execute_method_name && hook.hook_method_name == hook_method_name}
-    @@hooked << OpenStruct.new(strategy: :post, execute_method_name: execute_method_name, hook_method_name: hook_method_name)
+    @@hooked << OpenStruct.new(strategy: :pre, execute_method_name: execute_method_name, hook_method_name: hook_method_name)
     pre_method = instance_method(execute_method_name.id2name)
     hook_method = instance_method(hook_method_name.id2name)
     define_method(hook_method_name) do |*args|
